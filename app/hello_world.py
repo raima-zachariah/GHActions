58207d0770a9a86a00ce7133a656eb9097339bfa
raima-zachariah <raima.zachariah@yahoo.co.in>
2021-05-07 21:51:09 +0530

    add mining code

--- app/hello_world.py
+++ app/hello_world.py
@@ -1,6 +1,21 @@
 def greetings(name="World"):
     return f"Hello {name}"
 
+
+import hashlib
+
+
+def sha256(message):
+    return hashlib.sha256(message.encode("ascii")).hexdigest()
+
+
 def code_exc():
-    for i in range(3):
-        print(i)
+    difficulty = 1
+    message = "helloworld"
+    assert difficulty >= 1
+    prefix = "1" * difficulty
+    for i in range(1000):
+        digest = sha256(str(hash(message)) + str(i))
+        if digest.startswith(prefix):
+            print("after " + str(i) + " iterations found nonce: " + digest)
+        print(digest)
